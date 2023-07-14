b2ee7e271996cd302b27090ba39678c942b8a4eb
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-14 11:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+   let ans  = 0;
+	let p  = 1;
+	while(num > 0)
+		{
+			let dig =  num % 2;
+			num  = math.floor(num/2);
+			ans += dig * p ;
+			p = p * 10;
+		}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
+// 
